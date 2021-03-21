817057518f672c6b79151dd45b2de4fb74284756
Chaitanya748 <69597412+Chaitanya748@users.noreply.github.com>
2021-03-21 11:36:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,74 +9,75 @@ tapandbuket = loadImage("tapandbuket-.png")
 
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(windowWidth,windowHeight);
   gameState = "play"
   logGroup = new Group();
   
   var x = 50;
   for(var i = 0; i < 6; i = i+1){
-    var temp = createSprite(x,275, 40, 50)
+    var temp = createSprite(x,height - 200, 40, 50)
     logs.push(temp);
     logGroup.add(temp)
     logs[i].shapeColor = "yellow";
     x = x + 50;
   
   }
-  log2 = createSprite(80,170,30)
+  log2 = createSprite(280,190,30)
   logGroup.add(log2)
-  log3  = createSprite(115,110,100,30)
+  log3  = createSprite(315,130,100,30)
   logGroup.add(log3)
-  log4 = createSprite(615,110,100,30)
+  log4 = createSprite(865,110,100,30)
   logGroup.add(log4)
-  log5 = createSprite(650,175,30)
+  log5 = createSprite(900,175,30)
   logGroup.add(log5)
-  log6 = createSprite(370,176,450,30)
+  log6 = createSprite(600,176,450,30)
   logGroup.add(log6)
-  log7 = createSprite(8,200,15,400)
+  log7 = createSprite(10,height/2,15,height)
   logGroup.add(log7)
-  log8 = createSprite(792,200,15,400)
+  log8 = createSprite(width-10,height/2,15,height)
   logGroup.add(log8)
-  log9 = createSprite(400,392,800,15)
+  log9 = createSprite(width/2,height-10,width,15)
   logGroup.add(log9)
-  log10 = createSprite(148,50,5,70)
+  log10 = createSprite(269,55,5,70)
   logGroup.add(log10)
-  log11 = createSprite(180,13,70,5)
+  log11 = createSprite(302,20,70,5)
   logGroup.add(log11)
-  log12 = createSprite(212,50,5,70)
+  log12 = createSprite(334,55,5,70)
   logGroup.add(log12)
-  log13 = createSprite(244,87,70,5)
+  log13 = createSprite(367,89,70,5)
   logGroup.add(log13)
-  log14 = createSprite(280,110,5,50)
+  log14 = createSprite(400,115,5,50)
   logGroup.add(log14)
-  log15 = createSprite(377,138,200,5)
+  log15 = createSprite(500,138,200,5)
   logGroup.add(log15)
-  log16 = createSprite(553,55,5,70)
+  log16 = createSprite(735,55,5,70)
   logGroup.add(log16)
-  log17  = createSprite(585,20,70,5)
+  log17  = createSprite(767,20,70,5)
   logGroup.add(log17)
-  log18 = createSprite(618,55,5,70)
+  log18 = createSprite(800,55,5,70)
   logGroup.add(log18)
-  boy = createSprite(382,328,10,10)
+  boy = createSprite(382,height - 150,10,10)
   boy.addImage(boyimg);
   boy.scale=(0.3)
-  tapbucket = createSprite(410,49,30,30)
+  tapbucket = createSprite(580,82,30,30)
   tapbucket.addImage(tapandbuket);
   tapbucket.scale=(0.2)
   boy.debug=true
   boy.setCollider("rectangle",0,0,40,80);
 
   x = x +100;
-  
-  for(var i = 6; i <13; i =i+1){
-    var block=createSprite(x, 275, 40, 50)
+  var i = 6
+  while (x<width- 50) { 
+    var block=createSprite(x, height-200, 40, 50)
     logGroup.add(block)
     logs.push(block);
     logs[i].shapeColor = "yellow";
     x = x + 50;
+    i=i+1
   }
 
-
-
+  
+  
  
 
   
@@ -102,7 +103,7 @@ function draw() {
     if (logGroup.isTouching(boy)) {
 
       boy.x=382
-      boy.y=328
+      boy.y=height - 150;
     }
 
     if (boy.isTouching(tapbucket)) {
@@ -110,7 +111,7 @@ function draw() {
     }
   } else if (gameState==="end") {
     textSize(30)
-    text("Congrats Level Completed", 200,200)
+    text("Congrats Level Completed", windowWidth/2,windowHeight/2)
     
   }
   
